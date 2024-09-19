988be879afb2b761454a56e59fd6b39a17525cf0
Michael McGarvey <71113885+MochaGamesDev@users.noreply.github.com>
2024-09-19 19:48:48 -0400

    Work on topic producer solution

--- Tech-Lab-On-Campus/Topic-Exchange/solution/producer_sol.py
+++ Tech-Lab-On-Campus/Topic-Exchange/solution/producer_sol.py
@@ -1,6 +1,7 @@
 from producer_interface import mqProducerInterface
 import pika
 import os
+import sys
 
 class mqProducer(mqProducerInterface):
     routing_key = ""
@@ -19,24 +20,39 @@ class mqProducer(mqProducerInterface):
 
     def setupRMQConnection(self) -> None:
         # Set-up Connection to RabbitMQ service
-        conParams = pika.URLParameters(os.environ['AMQP_URL'])
-        self.connection = pika.BlockingConnection(parameters=conParams)
+        connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
 
         # Establish Channel
         self.channel = self.connection.channel()
 
-        # Create the exchange if not already present
-        exchange = self.channel.exchange_declare(exchange=self.exchange_name)
+        # Create the topic exchange if not already present
+        exchange = self.channel.exchange_declare(exchange=self.exchange_name, exchange_type="topic")
 
         pass
 
     def publishOrder(self, message: str) -> None:
         # Basic Publish to Exchange
-        self.channel.basic_publish(
-            exchange=self.exchange_name,
-            routing_key=self.routing_key,
-            body="Hello World",
-        )
+        # self.channel.basic_publish(
+            # exchange=self.exchange_name,
+            # routing_key=self.routing_key,
+            # body="Hello World",
+        # )
+
+        # Create Appropiate Topic String
+
+        # Send serialized message or String
+        print("Name of the program using sys.argv[0]: ", sys.argv[0])
+        print("Length of arguments given including program name: ", len(sys.argv))
+        print("Argument list: ", sys.argv)
+        print("Argument list type: ", type(sys.argv))
+        print("Give the first argument (after program name): ", sys.argv[1])
+
+        routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
+        message = ' '.join(sys.argv[2:]) or 'Hello World!'
+        channel.basic_publish(
+            exchange='topic_logs', routing_key=routing_key, body=message)
+        print(f" [x] Sent {routing_key}:{message}")
 
         # Close Channel
         self.channel.close()
